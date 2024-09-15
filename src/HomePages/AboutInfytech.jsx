8148be0cd003962629625bb5201bc7f162ce8579
krish2592 <sachink9225s@gmail.com>
2024-09-15 16:59:56 +0530

    updated about us and services

--- src/HomePages/AboutInfytech.jsx
+++ src/HomePages/AboutInfytech.jsx
@@ -6,23 +6,42 @@ import '../HomePages/aboutInfytech.css'
 const AboutInfytech = () => {
     return (
         <html>
-            <Header/>
+            <Header />
             <body className="about-us-container">
                 <div><h1>About US</h1>
-                    <p><strong>Infytech Loop AI Private Limited</strong> is a private limited company working in the area of education and mep consultancy. Maahaul mission is to provide a carrier guarantee with no or minimum time loss, coupled with a strong basic foundation, early understanding of student capabilities through data analysis, and the right career guidance. We are dedicated to empowering individuals to excel in carrier-oriented examinations, government jobs, and private job opportunities through our revolutionary education and engineering solutions. MEP Consultants team is highly expertise in MEP Designing for Home, Residential Building, Commercial Building, Hospital, Hotel, Railway Station, Bus Stands, Substations, Switchyards and Various Industries.</p>
+                    <p><strong>At Infytech MEP Design Consultants, </strong>
+                        we are passionate about delivering innovative and
+                        sustainable solutions in Mechanical, Electrical, and Plumbing (MEP) design.
+                        Our team of young and professional designers ensures a unique, project-specific,
+                        practical, and efficient design approach, with a major emphasis on minimizing energy
+                    waste—not only in theory but in real-life operations.</p>
                     <div></div>
                 </div>
-                <div><h2>MEP Consultants</h2>
+                <div><h2>Our Missions</h2>
+                    <p>Our mission is to deliver transformative impact through modern techniques, with a focus on digital twinning to lead the change in building design.</p>
                     <div><h3>Our Areas of Expertise</h3></div>
-                    <p><strong>1. MEP Engineering Solutions:</strong> Infytech MEP Consultants specializes in designing Heating, Ventilation, and Air Conditioning (HVAC), Electrical, Low voltage, Public Health Engineering (PHE), and Fire Fighting solutions. Our engineering services cater to a diverse range of projects, including residential, commercial, hotels, hospitals, metro stations, bus stands, substations, switchyards, and various industries.</p>
+                    <p><strong>1. MEP Engineering Services: </strong>We specialize in designing Heating, Ventilation, and Air Conditioning (HVAC), Electrical, Low Voltage, Public Health Engineering (PHE), and Fire Fighting solutions. Our engineering services cater to a diverse range of projects, including:</p>
+                    <p>Data centers</p>
+                    <p>Industrial facilities</p>
+                    <p>Laboratories</p>
+                    <p>Residential and commercial buildings</p>
+                    <p>Offices</p>
+                    <p>Hotels and hospitality</p>
+                    <p>Mixed-use developments</p>
+                    <p>Educational and institutional facilities</p>
+                    <p>Restaurants and bars</p>
+                    <p>Metro stations</p>
+                    <p>Bus terminals</p>
+                    <p>Substations and switchyards</p>
+                    <p>Various other industries</p>
                     <p><strong>2. Project Management Consultancy:</strong> With extensive experience in project management, we offer expert consultancy services for building projects. Our team ensures efficient and successful project execution, adhering to timelines and budget constraints.</p>
                     <p><strong>3. Green Energy and Sustainability:</strong> Infytech Loop AI is committed to promoting green energy and sustainable practices. We actively engage in projects that contribute to environmental preservation and energy efficiency.</p>
                     <p><strong>4. Sales and Purchase of Goods:</strong> Beyond our core services, we also engage in the sales and purchase of goods, offering a wide range of products that meet the needs of our clients.</p>
                 </div>
                 <div>At Infytech Loop AI Private Limited, we are driven by innovation, excellence, and a passion for education and engineering solutions. Our dedicated team is committed to provide outstanding services to our clients, ensuring their success and growth. Join us on our journey towards a brighter future through education and sustainable engineering solutions.</div>
-                
+
             </body>
-            <Footer/>
+            <Footer />
         </html>
     );
 }
--- src/HomePages/Services.jsx
+++ src/HomePages/Services.jsx
@@ -17,7 +17,7 @@ const Services = () => {
                 </div>
                 <div className="service-container">
                     <div className="service-1-row-1-container">
-                        <h2 id="ser1-row1-main-heading">Services Engineering Design</h2>
+                        <h2 id="ser1-row1-main-heading">Engineering Design</h2>
                         <div className="service-1-row-1">
                             <div className="service-1-col-1">
                                 <h3 id="ser1-col1-main-cont1">Engineering</h3>
@@ -25,8 +25,6 @@ const Services = () => {
                                 <div id="ser1-col1-sub-cont1">➔ Electrical</div>
                                 <div className="" id="ser1-col1-sub-cont2">➔ Emergency Power</div>
                                 <div className="" id="ser1-col1-sub-cont3">➔ HVAC</div>
-                                <div className="" id="ser1-col1-sub-cont4">➔ IAQ Consulting</div>
-                                <div className="" id="ser1-col1-sub-cont5">➔ Vertical Transportation</div>
                                 </div>
                             </div>
                             <div className="service-1-col-2">
@@ -58,8 +56,7 @@ const Services = () => {
                                 <div id="ser2-col1-sub-cont3">➔ Climatic Studies</div>
                                 <div id="ser2-col1-sub-cont4">➔ Day Lighting Simulations</div>
                                 <div className="" id="ser2-col1-sub-cont5">➔ Energy Modelling</div>
-                                <div className="" id="ser2-col1-sub-cont6">➔ Façade Optimization</div>
-                                <div className="" id="ser2-col1-sub-cont7">➔ Heat Island Assessmen</div>
+                                <div className="" id="ser2-col1-sub-cont7">➔ Heat Island Assessment</div>
                                 <div className="" id="ser2-col1-sub-cont8">➔ Renewable Energy</div>
                                 <div className="" id="ser2-col1-sub-cont9">➔ Solar & Wind Studies</div>
                                 <div className="" id="ser2-col1-sub-cont10">➔ Thermal Comfort Analysis</div>
@@ -84,7 +81,6 @@ const Services = () => {
                                 <h3 id="ser3-col1-main-cont1">Property Condition</h3>
                                 <div className="ser3-col-sub-cont">
                                 <div id="ser3-col1-sub-cont1">➔ Mechanical & Electrical Systems</div>
-                                <div className="" id="ser3-col1-sub-cont2">➔ Structural System</div>
                                 <div className="" id="ser3-col1-sub-cont3">➔ Environmental</div>
                                 <div className="" id="ser3-col1-sub-cont4">➔ Review Property's "As Build" Condition</div>
                                 <div className="" id="ser3-col1-sub-cont5">➔ Review System Warranties</div>
@@ -100,25 +96,24 @@ Permit</div>
                                 <div className="" id="ser3-col2-sub-cont1">➔ BMS Systems Audit</div>
                                 <div id="ser3-col2-sub-cont2">➔ Calibrated Energy Model</div>
                                 <div className="" id="ser3-col2-sub-cont3">➔ Energy Audit</div>
-                                <div className="" id="ser3-col2-sub-cont4">➔ IAQ Audit</div>
                                 <div id="ser3-col2-sub-cont5">➔ Sustainability Audit</div>
                                 <div className="" id="ser3-col2-sub-cont6">➔ Water Audit</div>
                                 </div>
                             </div>
-                            <div className="service-3-col-3">
+                            {/* <div className="service-3-col-3">
                                 <h3 id="ser3-col3-main-cont3">Commissioning</h3>
                                 <div className="ser3-col-sub-cont">
                                 <div className="" id="ser3-col3-sub-cont1">➔ Equipment Performance Validation</div>
                                 <div className="" id="ser3-col3-sub-cont2">➔ Testing, Adjusting & Balancing (TAB)</div>
                                 </div>
-                            </div>
+                            </div> */}
                         </div>
                     </div>
                     <div class="service-4-row-4-container">
-                        <h2 id="ser4-row4-main-heading">BIM Compliant And Undertake Following Task:</h2>
+                        <h2 id="ser4-row4-main-heading">BIM Compliant:</h2>
                         <div className="service-4-row-4">
                         <div className="service-4-col-1">
-                                <h3 id="ser4-col1-main-cont1">Property Condition</h3>
+                                {/* <h3 id="ser4-col1-main-cont1"></h3> */}
                                 <div className="ser4-col-sub-cont">
                                 <div id="ser4-col1-sub-cont1">➔ BIM Modeling</div>
                                 <div className="" id="ser4-col1-sub-cont2">➔ Design Integration</div>
